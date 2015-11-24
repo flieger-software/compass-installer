16fa81163a6842caf25f0d82dda893ad4aa8b443
w.souza <w.souza@fliegersoftware.de>
2015-11-24 18:41:10 -0200

    adicionar script e colocar descrição de como usar

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# compass-installer
-installs the compass environment
+## compass-installer
+This script installs the compass environment into your machine
+
+### Give executable permission to the file
+```sh
+sudo chmod +x infonavit-env.sh
+```
+
+## Then run it
+```sh
+./infonavit-env.sh
+```
---
+++ infonavit-env.sh
@@ -0,0 +1,48 @@
+#!/bin/sh -e
+
+blue='\033[0;34m'
+green='\033[0;32m'
+reset='\033[0m'
+
+# Check if Ruby is installed
+ruby=$(which ruby 2>/dev/null | grep -v "not found" | wc -l)
+
+
+if [ !$ruby -eq 0 ] ; then
+	printf "${blue}Do you want to install Ruby? [yes/no]${reset}\n"
+	read answer
+
+	if [ $answer = yes ] ; then
+		printf "${green}============================== Installing Ruby ==============================${reset}\n"
+	fi
+fi
+
+
+# Check if sass is installed
+sass=$(gem list sass -i)
+
+if [ !$sass ] ; then
+	printf "${blue}Do you want to install sass? [yes/no]${reset}\n"
+	read answer
+
+	if [ $answer = yes ] ; then
+		printf "${green}============================== Installing SASS ==============================${reset}\n"
+		sudo su -c "gem install sass"
+	fi
+fi
+
+
+# Check if compass is installed
+compass=$(gem list compass -i)
+
+if [ !$compass ] ; then
+	printf "${blue}Do you want to install compass? [yes/no]${reset}\n"
+	read answer
+
+	if [ $answer = yes ] ; then
+		printf "${green}============================== Installing Compass ==============================${reset}\n"
+		sudo su -c "gem install compass"
+	fi
+fi
+
+exit
